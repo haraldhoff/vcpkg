05652ec31512553ab7a976a6ef440ce1c4660ba8
Harald Hoff <harald.hoff@ffi.no>
2019-11-25 16:01:05 +0100

    ...

--- vs-tutorials/boost-lamda/boost-lamda.cpp
+++ vs-tutorials/boost-lamda/boost-lamda.cpp
@@ -1,5 +1,7 @@
 // https://www.boost.org/doc/libs/1_64_0/more/getting_started/windows.html
 
+// Usage: Enter some number in the console and press enter
+
 #include <boost/lambda/lambda.hpp>
 #include <iostream>
 #include <iterator>
