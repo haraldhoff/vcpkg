67a37046c574d52a66465521e8f511188784b105
Harald Hoff <harald.hoff@ffi.no>
2019-11-25 15:54:48 +0100

    ...

--- upgrade-packages.bat
+++
@@ -1,6 +0,0 @@
-echo ------------------------------------------------------------------------------------------ >> vcpkg-info\upgrade-%1.txt
-call e vcpkg-info\upgrade-%1.txt
-vcpkg\vcpkg.exe upgrade --no-dry-run >> vcpkg-info\upgrade-%1.txt
-ping -n 3 127.0.0.1 > nul
-echo ------------------------------------------------------------------------------------------ >> vcpkg-info\upgrade-%1.txt
-dir vcpkg\installed\x64-windows\bin >> vcpkg-info\upgrade-%1.txt
