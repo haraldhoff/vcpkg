a95fdf8bddc6d258bc0e391cd03b3df9606ae413
Harald Hoff <harald@hoffelec.com>
2019-11-25 23:00:18 +0100

    ...

--- install-package.bat
+++ install-package.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Installing package %1, logging output to vcpkg-info\%1.txt
 echo Please check logfile after build is complete
-mkdir vcpkg-info
+if not exist "vcpkg-info" mkdir vcpkg-info
 vcpkg.exe install %1:x64-windows >> vcpkg-info\%1.txt
