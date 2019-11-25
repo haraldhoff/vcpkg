63acc9b6d21fd3bb01766ea3cbe2ade487c167f1
Harald Hoff <harald.hoff@ffi.no>
2019-11-25 15:54:06 +0100

    ...

--- vs-tutorials/boost-lamda/boost-lamda.cpp
+++ vs-tutorials/boost-lamda/boost-lamda.cpp
@@ -1,6 +1,15 @@
+// https://www.boost.org/doc/libs/1_64_0/more/getting_started/windows.html
+
+#include <boost/lambda/lambda.hpp>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
+
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << " ");
 }
