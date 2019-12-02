ab8412d9e650f03f854b89204c8d05fb65935ba4
Harald Hoff <harald@hoffelec.com>
2019-12-02 15:58:03 +0100

    ...

--- vs-tutorials/opencv4/opencv4.cpp
+++ vs-tutorials/opencv4/opencv4.cpp
@@ -1,13 +1,30 @@
-// https://github.com/jbeder/yaml-cpp/wiki/Tutorial
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
-#include "yaml-cpp/yaml.h"
-#include <iostream>
+using namespace cv;
 
-int main()
-{
-	YAML::Node primes = YAML::Load("[2, 3, 5, 7, 11]");
-	for (std::size_t i = 0; i < primes.size(); i++)
-	{
-		std::cout << primes[i].as<int>() << "\n";
-	}
+int main(int argc, char** argv) {
+
+	//create a gui window:
+	namedWindow("Output", 1);
+
+	//initialize a 120X350 matrix of black pixels:
+	Mat output = Mat::zeros(120, 350, CV_8UC3);
+
+	//write text on the matrix:
+	putText(output,
+		"Hello World :)",
+		cvPoint(15, 70),
+		FONT_HERSHEY_PLAIN,
+		3,
+		cvScalar(0, 255, 0),
+		4);
+
+	//display the image:
+	imshow("Output", output);
+
+	//wait for the user to press any key:
+	waitKey(0);
+
+	return 0;
 }
