f65f0db7a0917285c68be9c2765677f1c0467879
Harald Hoff <harald@hoffelec.com>
2020-01-20 19:00:04 +0100

    ...

--- vs-tutorials/boost-asio/boost-asio.cpp
+++ vs-tutorials/boost-asio/boost-asio.cpp
@@ -1,14 +1,20 @@
 // https://www.boost.org/doc/libs/1_71_0/doc/html/boost_asio/tutorial/tuttimer1.html
 
 #include <iostream>
+#include <chrono>
 #include <boost/asio.hpp>
+
+using namespace std;
 
 int main()
 {
+	cout << "Waiting 3 seconds -- calling boost::asio::chrono::seconds(3)" << endl;
+	auto t1 = chrono::high_resolution_clock::now();
 	boost::asio::io_context io;
-	boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+	boost::asio::steady_timer t(io, boost::asio::chrono::seconds(3));
 	t.wait();
-	std::cout << "Hello, world!" << std::endl;
-
+	auto t2 = chrono::high_resolution_clock::now();
+	auto ms = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
+	cout << "Finished! --  std::chrono high precision timers says duration is " << ms << " [ms]" << endl;
 	return 0;
 }
