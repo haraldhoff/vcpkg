e835fbfa10ed4af7f0a93b5613f98aa0b225a6cf
Harald Hoff <harald@hoffelec.com>
2020-01-20 19:55:31 +0100

    ...

---
+++ vs-tutorials/nana-cmake/generate-vs2019-sln.bat
@@ -0,0 +1,8 @@
+
+@echo off
+if "%VCPKG_ROOT%"== "" (
+  echo VCPKG_ROOT not set, using default C:\opt\vcpkg
+  set VCPKG_ROOT=C:\opt\vcpkg
+)
+
+cmake -G "Visual Studio 15 2019" -A x64 -S . -B build
