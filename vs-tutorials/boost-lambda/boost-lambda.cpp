ddd1976a37ff71f49614ecad292e4d886cdde426
Harald Hoff <harald@hoffelec.com>
2020-01-20 19:07:09 +0100

    ...

--- vs-tutorials/boost-lambda/boost-lambda.cpp
+++ vs-tutorials/boost-lambda/boost-lambda.cpp
@@ -1,17 +1,19 @@
 // https://www.boost.org/doc/libs/1_71_0/more/getting_started/windows.html
 
-// Usage: Enter some number in the console and press enter
-
 #include <boost/lambda/lambda.hpp>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
 
+using namespace std;
+using namespace boost::lambda;
+
 int main()
 {
-	using namespace boost::lambda;
-	typedef std::istream_iterator<int> in;
+	typedef istream_iterator<int> in;
 
-	std::for_each(
-		in(std::cin), in(), std::cout << (_1 * 3) << " ");
+	cout << "Usage: Enter some numbers in the console with space between and press enter" << endl;
+
+	for_each(
+		in(cin), in(), cout << (_1 * 3) << " ");
 }
