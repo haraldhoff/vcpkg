11a7ea9bb0a00b60948abf4cd43ecafb6137957d
Harald Hoff <harald@hoffelec.com>
2020-10-15 16:41:59 +0200

    ...

--- install-package.bat
+++ install-package.bat
@@ -1,14 +1,19 @@
 @echo off
 
-if %cd% == "C:\opt\vcpkg" goto ok1
+if "%cd%" == "C:\opt\vcpkg" goto ok1
 echo This vcpkg git repository must be checked out to C:\opt\vcpkg in order to work properly
 goto out
 
 :ok1
+if not "%1" == "" goto ok2
+echo No package specified
+goto out
+
+:ok2
 if exist "vcpkg.exe" goto ok2:
 call bootstrap-vcpkg.bat
 
-:ok2
+:ok3:
 echo Installing package %1, logging output to vcpkg-info\%1.txt
 echo Please check logfile after build is complete
 if not exist "vcpkg-info" mkdir vcpkg-info
